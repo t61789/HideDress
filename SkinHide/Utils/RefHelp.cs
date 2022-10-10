81d831d519e1dae21841832806abd81946f78184
kmyuhkyuk <2451614940@qq.com>
2022-10-11 06:43:03 +0800

    Update

--- SkinHide/Utils/RefHelp.cs
+++ SkinHide/Utils/RefHelp.cs
@@ -45,7 +45,6 @@ namespace SkinHide.Utils
             var ilGen = dmd.GetILGenerator();
 
             Type[] parameterTypes;
-
             int num;
 
             if (!method.IsStatic)
@@ -89,7 +88,6 @@ namespace SkinHide.Utils
                     ilGen.Emit(OpCodes.Castclass, parameterTypes[i]);
                 }
             }
-
 
             if (method.IsStatic || !virtualCall)
             {
